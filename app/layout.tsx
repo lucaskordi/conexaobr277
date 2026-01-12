fd55f45b2262e5ed0fe3a7090836498dcce2b04a
lucaskordi <lucas.kordiaka@gmail.com>
2026-01-12 12:34:00 -0300

    Add Google Tag Manager integration to layout for enhanced tracking capabilities. Include both script and noscript tags to ensure proper functionality across different user scenarios.

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,11 +122,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5R7K3CWC');`
+        }} />
+        {/* End Google Tag Manager */}
+      </head>
       <GoogleAnalytics />
       <body
         className={`${montserrat.variable} antialiased`}
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5R7K3CWC"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
